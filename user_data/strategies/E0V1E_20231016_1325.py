ac24edf2d200e390d0321df1adc9f930dd585844
long <dxlongnh@gmail.com>
2023-10-16 13:25:28 +0000

    Auto commit

---
+++ user_data/strategies/E0V1E_20231016_1325.py
@@ -0,0 +1,111 @@
+from datetime import datetime
+import talib.abstract as ta
+import pandas_ta as pta
+from freqtrade.persistence import Trade
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame
+from freqtrade.strategy import DecimalParameter, IntParameter
+from functools import reduce
+
+
+class E0V1E_20231016_1325(IStrategy):
+    minimal_roi = {
+        "0": 0.02,
+        "120": 0.01
+    }
+
+    timeframe = '5m'
+
+    process_only_new_candles = True
+    startup_candle_count = 120
+    position_adjustment_enable = True
+
+    order_types = {
+        'entry': 'market',
+        'exit': 'market',
+        'emergency_exit': 'market',
+        'force_entry': 'market',
+        'force_exit': "market",
+        'stoploss': 'market',
+        'stoploss_on_exchange': False,
+
+        'stoploss_on_exchange_interval': 60,
+        'stoploss_on_exchange_market_ratio': 0.99
+    }
+
+    stoploss = -0.13
+
+    is_optimize_32 = True
+    buy_rsi_fast_32 = IntParameter(20, 70, default=46, space='buy', optimize=is_optimize_32)
+    buy_rsi_32 = IntParameter(15, 50, default=19, space='buy', optimize=is_optimize_32)
+    buy_sma15_32 = DecimalParameter(0.900, 1, default=0.942, decimals=3, space='buy', optimize=is_optimize_32)
+    buy_cti_32 = DecimalParameter(-1, 0, default=-0.86, decimals=2, space='buy', optimize=is_optimize_32)
+
+    def custom_stake_amount(self, pair: str, current_time: datetime, current_rate: float,
+                            proposed_stake: float, min_stake: float, max_stake: float,
+                            leverage: float, entry_tag: str, side: str,
+                            **kwargs) -> float:
+        return proposed_stake * 0.7
+
+    def adjust_trade_position(self, trade: Trade, current_time: datetime,
+                              current_rate: float, current_profit: float,
+                              min_stake: float, max_stake: float,
+                              current_entry_rate: float, current_exit_rate: float,
+                              current_entry_profit: float, current_exit_profit: float,
+                              **kwargs) -> float:
+        #Take half of profit
+        count_of_exits = trade.nr_of_successful_exits
+        if current_profit >= 0.02 and count_of_exits == 0:
+            return -0.5 * trade.stake_amount
+        #DCA
+        count_of_entries = trade.nr_of_successful_entries
+        filled_entries = trade.select_filled_orders(trade.entry_side)
+        initial_stake = filled_entries[0].cost
+
+        if current_profit < -0.05 and count_of_entries == 1 :
+            return initial_stake / 0.7 * 0.3
+        return None
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        # buy_1 indicators
+        dataframe['sma_15'] = ta.SMA(dataframe, timeperiod=15)
+        dataframe['cti'] = pta.cti(dataframe["close"], length=20)
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+        dataframe['rsi_fast'] = ta.RSI(dataframe, timeperiod=4)
+        dataframe['rsi_slow'] = ta.RSI(dataframe, timeperiod=20)
+
+        # profit sell indicators
+        stoch_fast = ta.STOCHF(dataframe, 5, 3, 0, 3, 0)
+        dataframe['fastk'] = stoch_fast['fastk']
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        conditions = []
+        dataframe.loc[:, 'enter_tag'] = ''
+
+        buy_1 = (
+                (dataframe['rsi_slow'] < dataframe['rsi_slow'].shift(1)) &
+                (dataframe['rsi_fast'] < self.buy_rsi_fast_32.value) &
+                (dataframe['rsi'] > self.buy_rsi_32.value) &
+                (dataframe['close'] < dataframe['sma_15'] * self.buy_sma15_32.value) &
+                (dataframe['cti'] < self.buy_cti_32.value)
+        )
+
+        conditions.append(buy_1)
+        dataframe.loc[buy_1, 'enter_tag'] += 'buy_1'
+
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x | y, conditions),
+                'enter_long'] = 1
+
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        dataframe.loc[(), ['exit_long', 'exit_tag']] = (0, 'long_out')
+
+        return dataframe
